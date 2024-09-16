cd7bfbf9d544ffd261d292cb7ca26b694b327535
Roman <roman.kolomin@gmail.com>
2024-09-16 15:16:21 +0700

    2024.09.16

--- Module_2/module_2_hard.py
+++ Module_2/module_2_hard.py
@@ -2,6 +2,7 @@ left_num = input('Введите шифр: ')
 left_num_possible_options = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 left_num_correct = True
 possible_pairs = []
+code = []
 
 # Checking if left_num is correct and making it int
 try:
@@ -13,17 +14,22 @@ if left_num not in left_num_possible_options or left_num_correct == False:
 else:
     left_num = int(left_num)
 
-# Making lists of pairs
-for i in range(1, max(left_num_possible_options)):
-    for j in range(1, max(left_num_possible_options)):
-        if (
-                i + j == left_num
-                and i != j
-                and [j, i] not in possible_pairs
-        ):
-            possible_pairs.append([i, j])
+    # Making lists of pairs
+    for i in range(1, max(left_num_possible_options)):
+        for j in range(1, max(left_num_possible_options)):
+            if (
+                    left_num % (i + j) == 0
+                    and i != j
+                    and [j, i] not in possible_pairs
+            ):
+                possible_pairs.append([i, j])
 
-# Making code from pairs
+    # Making code from pairs
+    for i in range(len(possible_pairs)):
+        code.append(int(''.join(map(str, possible_pairs[i]))))
+    code = int(''.join(map(str, code)))
 
-
-print(*possible_pairs)
+    print()
+    print('Введенный шифр: ', left_num)
+    print('Пары чисел: ', possible_pairs)
+    print('Введите пароль: ',code)
