acbc8ab5ae44af6fcf3a3f45c19be9682a09d90d
Roman <roman.kolomin@gmail.com>
2024-11-06 12:58:39 +0700

    2024.11.06

---
+++ Module_8/module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:
+        sum_ = a + b
+    except TypeError:
+        sum_ = str(a) + str(b)
+    return sum_
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
