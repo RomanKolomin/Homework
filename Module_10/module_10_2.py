6b3ddbcc73a3c49851b063a746f28a53e8fc4783
Roman <roman.kolomin@gmail.com>
2024-11-22 14:05:14 +0700

    2024.11.22

--- Module_10/module_10_2.py
+++ Module_10/module_10_2.py
@@ -22,6 +22,8 @@ class Knight(threading.Thread):
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-
 first_knight.start()
 second_knight.start()
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
